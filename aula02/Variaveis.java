fdf0a92eba81f01f209228a314299060b8513061
vinicius <viniciussantos45671@gmail.com>
2026-03-16 09:22:27 -0300

    Aula sobre variaveis

---
+++ aula02/Variaveis.java
@@ -0,0 +1,15 @@
+package aula02;
+
+public class Variaveis {
+    
+    public static void main(String[] args) {
+        String msgPt1 = "Olá";
+        String msgPt2 = "bratzinhos";
+        System.out.println(msgPt1 + " " + msgPt2);
+
+        int n1 = 1;
+        String n2 =  "2";
+        System.out.println(n1 + Integer.parseInt(n2));
+    }
+
+}
